be7472e43dcf6938f0d852f667eb62bb9639b907
Yinghan Chen <ychen@Yinghans-MacBook-Pro.local>
2021-11-01 20:22:42 -0400

    added script for keyboard camera capture and stream recording

---
+++ camera-main/ZED.py
@@ -0,0 +1,93 @@
+from collections import namedtuple
+import pyzed.sl as sl
+import numpy as np
+from time import sleep, time
+
+class ZEDCamera:
+   
+    def __init__(self, output=False, resolution='720', depth_mode='ultra', depth=True, color=True):
+        self.depth = depth
+        self.color = color
+        self.init = sl.InitParameters()
+        self.output_path = output
+        resolutions = {'720': sl.RESOLUTION.HD720,
+                       '1080':sl.RESOLUTION.HD1080,
+                       '2K'  :sl.RESOLUTION.HD2K}
+
+        depthModes = {'perf': sl.DEPTH_MODE.PERFORMANCE,
+                      'qual': sl.DEPTH_MODE.QUALITY,
+                      'ultra': sl.DEPTH_MODE.ULTRA,}
+
+        self.init.camera_resolution = resolutions[resolution]
+        self.init.depth_mode = depthModes[depth_mode]
+        self.cam = sl.Camera()
+        return
+
+    def _openCamera(self, totalAttempts=5):
+        for attempt in range(totalAttempts):
+            print('Opening ZED Camera...')
+            status = self.cam.open(self.init)
+            if status != sl.ERROR_CODE.SUCCESS:
+                print('\nTry {} out of {}'.format(attempt+1,totalAttempts))
+                print(repr(status))
+                if attempt == (totalAttempts-1):
+                    print('\n\n'+'-'*80)
+                    print('Failed to open ZED')
+                    print('Please Unplug the ZED and plug it back in!')
+                    return False
+            else:
+                return True
+
+    def __enter__(self):
+        totalAttempts = 5
+        for attempt in range(totalAttempts):
+            if self._openCamera() == True:
+                self.runtime = sl.RuntimeParameters()
+                varNames = []
+                if self.output_path:
+                    self.record_param = sl.RecordingParameters(compression_mode=sl.SVO_COMPRESSION_MODE.H264, video_filename=self.output_path)
+                    err = self.cam.enable_recording(self.record_param)
+                if self.depth == True:
+                    self.mat_depth = sl.Mat()
+                    varNames.append('depth')
+                if self.color == True:
+                    self.mat_color = sl.Mat()
+                    varNames.append('color')
+                varNames.append('timestamp')   
+                self.Album = namedtuple('Album', varNames)
+                return self
+            else:
+                sleep(5)
+        raise IOError('Camera could not be opened, please try power cycling the ZED')
+    
+    def __exit__(self, exc_type, exc_value, traceback):
+        print('Closing ZED...')
+        self.cam.disable_recording()
+    
+    def startStream(self):
+        self.__enter__()
+    
+    def closeStream(self):
+        self.__exit__(None, None, None)
+        
+    def takePicture(self, emptyBuffer=False):
+        """
+        Returns an Album namedtuple.
+        This method waits for a valid frame and builds the album
+        to be returned.
+        """
+        start = time()
+        status = self.cam.grab(self.runtime)
+        while True:
+            if status == sl.ERROR_CODE.SUCCESS:
+                svo_image = sl.Mat()
+                svo_depth = sl.Mat()
+                self.cam.retrieve_image(svo_depth, sl.VIEW.DEPTH)
+                self.cam.retrieve_image(svo_image, sl.VIEW.SIDE_BY_SIDE)
+                color_image = np.asanyarray(svo_image.get_data())
+                depth_image = np.asanyarray(svo_depth.get_data())
+                return self.Album(depth_image, color_image, time())
+            elif (time() - start) > 1:
+                raise TimeoutError('The ZED is taking longer than 1 sec')
+                
+
---
+++ camera-main/cameraRecord.py
@@ -0,0 +1,70 @@
+import sys
+import cv2
+import pickle as pkl
+from time import sleep, time
+
+
+OUTPUT_FILE = 'saved_{}.pkl'
+KEYBOARD_CAPTURE = False
+
+
+if __name__ == '__main__':
+    frames_saved, records = 0, {}
+    recordType = "stream"
+    key = input("Keyboard Capture(Y/n)?:\n")
+    if key.lower()=='y':
+        print('\nPress Enter to Capture and q to quit.')
+        KEYBOARD_CAPTURE=True
+        recordType = "captures"
+    try:
+        from ZED import ZEDCamera
+        zed_cam = ZEDCamera(resolution='1080')
+        zed_cam.startStream()
+        zed_album = zed_cam.takePicture()
+        zed_width = zed_album.color.shape()[1]
+        records['ZED_L'], records['ZED_R'] = [], []
+    except:
+        print("\nZED Camera Not Connected!")
+        
+    try:
+        from realSense import RealSense2
+        rs_cam = RealSense2()
+        reel = rs_cam.videoStream()
+        rs_album = reel.send(None)
+        records['RS'] = []
+    except:
+        print("\nRealSense Camera Not Connected!")
+    
+    # Tracking record duration
+    starting = time()
+    while True: 
+        try:
+            if KEYBOARD_CAPTURE:
+                key = input()
+                if key=='q': break
+                    
+            if 'RS' in records.keys():
+                rs_album = reel.send(None)
+                records['RS'].append(rs_album.color)
+                
+            if 'ZED_L' in records.keys():
+                zed_album = zed_cam.takePicture()
+                records['ZED_L'].append(zed_album.color[:,:zed_width,:3])
+                records['ZED_R'].append(zed_album.color[:,zed_width:,:3])
+                
+            frames_saved += 1
+            print("\n{} Frames Captured".format(frames_saved))
+            
+        except KeyboardInterrupt:
+            if KEYBOARD_CAPTURE:
+                print("\nSession Terminated.")
+            else:
+                print("\nStream Terminated with Duration: {}s".format(round(time() - starting, 1)))
+            break
+
+    if 'ZED_L' in records.keys(): zed_cam.closeStream()
+    if 'RS' in records.keys(): rs_cam.closeStream()
+
+    with open(OUTPUT_FILE.format(recordType), 'wb') as file:
+        pkl.dump(records, file)
+
