01cb899a98abd75262cf7557b88f21e2babbbedd
Luke Yao <x35yao@uwaterloo.ca>
2021-11-04 15:57:08 -0400

    rename the output in the format: {time}_saved_{recordType}

--- camera-main/cameraRecord.py
+++ camera-main/cameraRecord.py
@@ -4,7 +4,7 @@ import pickle as pkl
 from time import sleep, time
 
 
-OUTPUT_FILE = 'saved_{}.pkl'
+OUTPUT_FILE = './saved_frames/{}_saved_{}.pkl'
 KEYBOARD_CAPTURE = False
 
 
@@ -25,7 +25,7 @@ if __name__ == '__main__':
         records['ZED_L'], records['ZED_R'] = [], []
     except:
         print("\nZED Camera Not Connected!")
-        
+
     try:
         from realSense import RealSense2
         rs_cam = RealSense2()
@@ -34,27 +34,27 @@ if __name__ == '__main__':
         records['RS'] = []
     except:
         print("\nRealSense Camera Not Connected!")
-    
+
     # Tracking record duration
     starting = time()
-    while True: 
+    while True:
         try:
             if KEYBOARD_CAPTURE:
                 key = input()
                 if key=='q': break
-                    
+
             if 'RS' in records.keys():
                 rs_album = reel.send(None)
                 records['RS'].append(rs_album.color)
-                
+
             if 'ZED_L' in records.keys():
                 zed_album = zed_cam.takePicture()
                 records['ZED_L'].append(zed_album.color[:,:zed_width,:3])
                 records['ZED_R'].append(zed_album.color[:,zed_width:,:3])
-                
+
             frames_saved += 1
             print("\n{} Frames Captured".format(frames_saved))
-            
+
         except KeyboardInterrupt:
             if KEYBOARD_CAPTURE:
                 print("\nSession Terminated.")
@@ -65,6 +65,5 @@ if __name__ == '__main__':
     if 'ZED_L' in records.keys(): zed_cam.closeStream()
     if 'RS' in records.keys(): rs_cam.closeStream()
 
-    with open(OUTPUT_FILE.format(recordType), 'wb') as file:
+    with open(OUTPUT_FILE.format(str(int(starting)), recordType), 'wb') as file:
         pkl.dump(records, file)
-
