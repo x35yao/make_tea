1a5e28d48f6ec6aeb9bd68dedf78a1a54ed0e2b2
Luke <x35yao@uwaterloo.ca>
2021-11-26 14:13:47 -0500

    create directories for each video id

--- camera-main/frams2videos.py
+++ camera-main/frams2videos.py
@@ -3,13 +3,13 @@ import imageio
 import pickle as pkl
 import numpy as np
 import cv2
+from pathlib import Path
 
 input_dir = './saved_frames/'
 output_dir = './videos/'
 
 filenames = glob(input_dir + '*.pkl')
 for filename in filenames:
-    print(filename)
     with open(filename, 'rb') as file:
         album = pkl.load(file)
     zed_r = np.array(album['ZED_R'])
@@ -18,7 +18,14 @@ for filename in filenames:
     for i in range(zed_r.shape[0]):
         zed_r[i] = zed_r[i][...,::-1].copy()
         zed_l[i] = zed_l[i][...,::-1].copy()
-    video_name_left = output_dir + 'left_' + filename.strip(input_dir) + '.mp4'
-    video_name_right = output_dir + 'right_' + filename.strip(input_dir) + '.mp4'
+    video_ind = ''.join(list(filter(str.isdigit, filename)))
+    try:
+        Path(output_dir + video_ind).mkdir(exist_ok = False)
+    except FileExistsError:
+        print(output_dir + video_ind + 'already exists')
+    video_name_left = output_dir + video_ind + '/left''.mp4'
+    video_name_right = output_dir + video_ind + '/right''.mp4'
+
+
     imageio.mimwrite(video_name_left, zed_l , fps = 20)
     imageio.mimwrite(video_name_right, zed_r , fps = 20)
