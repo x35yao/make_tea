9b73dfb255787981656a59fff9b094a74c4f5996
Luke Yao <x35yao@uwaterloo.ca>
2022-11-10 12:38:13 -0500

    process quaternions to force it to be smooth

--- trajectory_modeling/quaternion_metric.py
+++ trajectory_modeling/quaternion_metric.py
@@ -1,5 +1,40 @@
 import numpy as np
 from numpy.linalg import norm as l2norm
+from scipy import ndimage
+
+
+def force_rotation_axis_direction(quat, axis=[1, 0, 0]):
+    vect = quat[:3]
+    dot_product = vect @ axis
+    if dot_product > 0:
+        return quat
+    else:
+        return -quat
+
+
+def force_smooth_quats( quats):
+    for i, quat_current in enumerate(quats):
+        if i == 0:
+            pass
+        else:
+            quat_previous = quats[i - 1]
+            result = quat_current[:3] @ quat_previous[:3]
+            if result > 0:
+                quats[i] = quat_current
+            else:
+                quats[i] = -quat_current
+    return quats
+
+
+def process_quaternions(quats, sigma = None):
+    init_quat = force_rotation_axis_direction(quats[0])
+    quats[0] = init_quat
+    quats_new = force_smooth_quats(quats)
+    if sigma != None:
+        quats_filtered = ndimage.gaussian_filter(quats_new, sigma=sigma)
+        return quats_filtered
+    else:
+        return quats_new
 
 
 def norm_diff_quat(q1, q2):
