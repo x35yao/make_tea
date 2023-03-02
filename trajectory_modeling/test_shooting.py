4517dc52f13d283dd80bba5a967505a036b20c9c
Luke Yao <x35yao@uwaterloo.ca>
2023-03-02 13:12:50 -0500

    add wiping data

--- trajectory_modeling/test_shooting.py
+++ trajectory_modeling/test_shooting.py
@@ -67,36 +67,42 @@ if __name__ == '__main__':
         # print(f'The outliers for individual {individual} are {outlier_individual}')
         outliers += outlier_individual
     outliers = list(set(outliers))
-    demos_not_detected = ['882778', '882777', '882774', '882816', '882792']
-    bad_demos = outliers + ['882782', '882786']
+    # demos_not_detected = ['882778', '882777', '882774', '882816', '882792']
+    # bad_demos = outliers + ['882782', '882786']
+    bad_demos_middle = ['882775']
+    bad_demos_left = ['882779', '882784', '882785','882786', '882793', '882789', '882810', '882796', '882797', '882807']
+    bad_demos = bad_demos_middle + bad_demos_left + outliers
 
     demos = sorted(list(HTs_obj_in_ndi['global'].keys()))
+
+    middle_pool = [d for d in demos[:8] if d not in bad_demos]
+    left_pool = [d for d in demos[8:26] if d not in bad_demos]
+    right_pool = [d for d in demos[26:] if d not in bad_demos]
+
     train_demos_pool = [demo for demo in demos if demo not in bad_demos]
     test_demos_pool = [demo for demo in demos if demo not in bad_demos]
     # Train model
     print(f'The number of training pool is: {len(train_demos_pool)}')
-    print(f'The number of outliers is: {len(outliers)}')
+    # print(f'The number of outliers is: {len(outliers)}')
     data.dims = ['x', 'y', 'z']
     n_dims = len(data.dims)
 
-    demos_total = sorted(list(gripper_trajs_in_ndi.keys()))
-
-    middle_pool = [d for d in demos_total[:10] if d in train_demos_pool]
-    left_pool = [d for d in demos_total[10:20] if d in train_demos_pool]
-    right_pool = [d for d in demos_total[30:40] if d in train_demos_pool]
     d1_pos, d2_pos, d3_pos, d4_pos = [], [], [], []
     d1_ori, d2_ori, d3_ori, d4_ori = [], [], [], []
     for i in trange(n_tests):
-        train_left = random.sample(left_pool, k=5)
-        train_middle = random.sample(middle_pool, k=1)
-        train_right = random.sample(right_pool, k=1)
-        # train_demos = train_left + train_middle + train_right
-        train_demos = train_left
-        # test_demos_pool_updated = [demo for demo in test_demos_pool if demo not in train_demos]
+        train_left = random.sample(left_pool, k=2)
+        train_middle = random.sample(middle_pool, k=2)
+        train_right = random.sample(right_pool, k=2)
+        train_demos = train_left + train_middle + train_right
+        # train_demos = train_left
+        test_demos_pool_updated = [demo for demo in test_demos_pool if demo not in train_demos]
         # test_demos = random.sample(test_demos_pool_updated, k=1)
         right_pool_updated = [demo for demo in right_pool if demo not in train_demos]
-        test_demos = random.sample(right_pool, k=1)
+        middle_pool_updated = [demo for demo in middle_pool if demo not in train_demos]
+        left_pool_updated = [demo for demo in left_pool if demo not in train_demos]
+        test_demos = random.sample(test_demos_pool_updated, k=1)
         test_demo = test_demos[0]
+        print(test_demo)
 
         ground_truth = gripper_traj_in_obj['global'][test_demo][data.dims].to_numpy()
 
@@ -136,7 +142,7 @@ if __name__ == '__main__':
 
         H_puck_in_ndi = HTs_generalized_obj_in_ndi['puck'][test_demo]
         start = ground_truth[0].copy()
-        # start[:3] = H_puck_in_ndi[:-1, 3] + np.array([0, -10, 0])
+        # start[:3] = H_puck_in_ndi[:-1, 3] + np.array([-250, -10, 0])
 
         model_pmp.condition(0, 1, q = start, ignore_Sy=False)
         mu_mean_pmp, sigma_mean_pmp = model_pmp.pmp.marginal_w(t_pmp)
@@ -159,9 +165,11 @@ if __name__ == '__main__':
                 model_tp_gmm = gmm.GMM(nb_states=nb_states, nb_dim=data_per_frame_gmm.shape[1])
                 model_tp_gmm.em(data_per_frame_gmm, reg=1e-8, maxiter=200, verbose=False)
                 tp_gmms[individual] = model_tp_gmm
-
-            mu_mean_tp_gmm, sigma_mean_tp_gmm = predict1(tp_gmms, t_gmm, test_demo, HTs_generalized_obj_in_ndi,
+            try:
+                mu_mean_tp_gmm, sigma_mean_tp_gmm = predict1(tp_gmms, t_gmm, test_demo, HTs_generalized_obj_in_ndi,
                                                          ['puck', 'net'])
+            except ValueError:
+                continue
             d = get_position_difference_per_step(ground_truth[:, :3], mu_mean_tp_gmm[:, :3])
             d_mean = np.mean(d)
             if d_mean < average_dist_min:
@@ -173,22 +181,22 @@ if __name__ == '__main__':
         d3_pos.append(pos_temp)
         d3_ori.append(ori_temp)
 
-        ### Naive model
-        naives = {}
-        for individual in sorted(individuals):
-            data_per_frame_naive, times = prepare_data_for_pmp(gripper_traj_in_generalized_obj, individual,
-                                                               train_demos,
-                                                               data.dims)
-            naive_model = naive.Naive()
-            naive_model.train(data_per_frame_naive, times)
-            naives[individual] = naive_model
-        mu_mean_naive, sigma_mean_naive = predict1(naives, t_pmp, test_demo, HTs_generalized_obj_in_ndi,
-                                                   individuals)
-        mu_pos4 = mu_mean_naive[:,:3]
-        mu_ori4 = mu_mean_naive[:,3:]
-        d = get_position_difference_per_step(ground_truth[:, :3], mu_pos4)
-        d4_pos.append([d[0], d[-1], np.mean(d)])
-        d4_ori.append(norm_diff_quat(ground_truth[:, 3:], mu_ori4))
+        # ### Naive model
+        # naives = {}
+        # for individual in sorted(individuals):
+        #     data_per_frame_naive, times = prepare_data_for_pmp(gripper_traj_in_generalized_obj, individual,
+        #                                                        train_demos,
+        #                                                        data.dims)
+        #     naive_model = naive.Naive()
+        #     naive_model.train(data_per_frame_naive, times)
+        #     naives[individual] = naive_model
+        # mu_mean_naive, sigma_mean_naive = predict1(naives, t_pmp, test_demo, HTs_generalized_obj_in_ndi,
+        #                                            individuals)
+        # mu_pos4 = mu_mean_naive[:,:3]
+        # mu_ori4 = mu_mean_naive[:,3:]
+        # d = get_position_difference_per_step(ground_truth[:, :3], mu_pos4)
+        # d4_pos.append([d[0], d[-1], np.mean(d)])
+        # d4_ori.append(norm_diff_quat(ground_truth[:, 3:], mu_ori4))
 
     H_net_in_ndi = HTs_generalized_obj_in_ndi['net'][test_demo]
     net_position = H_net_in_ndi[:-1, 3]
@@ -236,12 +244,12 @@ if __name__ == '__main__':
         ax.plot(mu_pos3[-1, 2], mu_pos3[-1, 1], -mu_pos3[-1, 0], 'x',
                 color=line3[0].get_color(), label='end')
 
-        line4 = ax.plot(mu_pos4[:, 2], mu_pos4[:, 1], -mu_pos4[:, 0], '-', color='purple',
-                        label='Naive')
-        ax.plot(mu_pos4[0, 2], mu_pos4[0, 1], -mu_pos4[0, 0], 'o',
-                color=line4[0].get_color(), label='start')
-        ax.plot(mu_pos4[-1, 2], mu_pos4[-1, 1], -mu_pos4[-1, 0], 'x',
-                color=line4[0].get_color(), label='end')
+        # line4 = ax.plot(mu_pos4[:, 2], mu_pos4[:, 1], -mu_pos4[:, 0], '-', color='purple',
+        #                 label='Naive')
+        # ax.plot(mu_pos4[0, 2], mu_pos4[0, 1], -mu_pos4[0, 0], 'o',
+        #         color=line4[0].get_color(), label='start')
+        # ax.plot(mu_pos4[-1, 2], mu_pos4[-1, 1], -mu_pos4[-1, 0], 'x',
+        #         color=line4[0].get_color(), label='end')
 
         ax.plot(net_position[2], net_position[1], -net_position[0], '*',
                 color='gold')
@@ -272,51 +280,51 @@ if __name__ == '__main__':
 
 
         #### Plot Orientation ##############
-        matplotlib.rcParams.update({'font.size': 18})
-        fig2, axes = plt.subplots(4, 1, figsize=(12, 8), sharex=True)
-        for demo in train_demos + [test_demo]:
-            quats = gripper_traj_in_obj['global'][demo][data.dims].to_numpy()[:, 3:]
-            quats_normalized = normalize_quats(quats)
-            if demo in train_demos:
-                for i, ax in enumerate(axes):
-                    ax.plot(quats[:, i], '--', color='grey', label='Training demos')
-            else:
-                for i, ax in enumerate(axes):
-                    ax.plot(quats[:, i], '-', color='red', label='Test demo')
+        # matplotlib.rcParams.update({'font.size': 18})
+        # fig2, axes = plt.subplots(4, 1, figsize=(12, 8), sharex=True)
+        # for demo in train_demos + [test_demo]:
+        #     quats = gripper_traj_in_obj['global'][demo][data.dims].to_numpy()[:, 3:]
+        #     quats_normalized = normalize_quats(quats)
+        #     if demo in train_demos:
+        #         for i, ax in enumerate(axes):
+        #             ax.plot(quats[:, i], '--', color='grey', label='Training demos')
+        #     else:
+        #         for i, ax in enumerate(axes):
+        #             ax.plot(quats[:, i], '-', color='red', label='Test demo')
+        #
+        # # Plot pmp and gmm prediction
+        # quats = mu_ori1
+        # quats_normalized = normalize_quats(quats)
+        # for i, ax in enumerate(axes):
+        #     ax.plot(quats_normalized[:, i], '-', color='blue', label='POP-ProMP')
+        #     ax.set_title(data.dims[i + 3])
+        #     if i == 3:
+        #         ax.set_xlabel('Time')
+        #
+        # quats = mu_ori2
+        # quats_normalized = normalize_quats(quats)
+        # for i, ax in enumerate(axes):
+        #     ax.plot(quats_normalized[:, i], '-', color='yellow', label='ProMP')
+        #     ax.set_title(data.dims[i + 3])
+        #     if i == 3:
+        #         ax.set_xlabel('Time (seconds)')
+        # # plt.legend(newHandles, newLabels, loc='lower left')
+        #
+        # quats = mu_ori3
+        # quats_normalized = normalize_quats(quats)
+        # for i, ax in enumerate(axes):
+        #     ax.plot(quats_normalized[:, i], '-', color='green', label='TP-GMM')
+        #     ax.set_title(data.dims[i + 3])
+        #     if i == 3:
+        #         ax.set_xlabel('Time (seconds)')
 
-        # Plot pmp and gmm prediction
-        quats = mu_ori1
-        quats_normalized = normalize_quats(quats)
-        for i, ax in enumerate(axes):
-            ax.plot(quats_normalized[:, i], '-', color='blue', label='POP-ProMP')
-            ax.set_title(data.dims[i + 3])
-            if i == 3:
-                ax.set_xlabel('Time')
-
-        quats = mu_ori2
-        quats_normalized = normalize_quats(quats)
-        for i, ax in enumerate(axes):
-            ax.plot(quats_normalized[:, i], '-', color='yellow', label='ProMP')
-            ax.set_title(data.dims[i + 3])
-            if i == 3:
-                ax.set_xlabel('Time (seconds)')
-        # plt.legend(newHandles, newLabels, loc='lower left')
-
-        quats = mu_ori3
-        quats_normalized = normalize_quats(quats)
-        for i, ax in enumerate(axes):
-            ax.plot(quats_normalized[:, i], '-', color='green', label='TP-GMM')
-            ax.set_title(data.dims[i + 3])
-            if i == 3:
-                ax.set_xlabel('Time (seconds)')
-
-        quats = mu_ori4
-        quats_normalized = normalize_quats(quats)
-        for i, ax in enumerate(axes):
-            ax.plot(quats_normalized[:, i], '-', color='purple', label='naive')
-            ax.set_title(data.dims[i + 3])
-            if i == 3:
-                ax.set_xlabel('Time (seconds)')
+        # quats = mu_ori4
+        # quats_normalized = normalize_quats(quats)
+        # for i, ax in enumerate(axes):
+        #     ax.plot(quats_normalized[:, i], '-', color='purple', label='naive')
+        #     ax.set_title(data.dims[i + 3])
+        #     if i == 3:
+        #         ax.set_xlabel('Time (seconds)')
         handles, labels = ax.get_legend_handles_labels()
         newHandles, newLabels = remove_repetitive_labels(handles, labels)
         # plt.legend(newHandles, newLabels, loc='lower left',  prop={'size': 10})
@@ -330,5 +338,5 @@ if __name__ == '__main__':
         f'2 : ProMP: This distance at the start: {np.mean(np.array(d2_pos), axis=0)[0]} mm, end: {np.mean(np.array(d2_pos), axis=0)[1]},  average: {np.mean(np.array(d2_pos), axis=0)[-1]} mm, average quaternion: {np.mean(np.array(d2_ori))}')
     print(
         f'3 : TP-GMM: This distance at the start: {np.mean(np.array(d3_pos), axis=0)[0]} mm, end: {np.mean(np.array(d3_pos), axis=0)[1]}, average: {np.mean(np.array(d3_pos), axis=0)[-1]} mm, average quaternion: {np.mean(np.array(d3_ori))}')
-    print(
-        f'3 : TP-GMM: This distance at the start: {np.mean(np.array(d4_pos), axis=0)[0]} mm, end: {np.mean(np.array(d4_pos), axis=0)[1]}, average: {np.mean(np.array(d4_pos), axis=0)[-1]} mm, average quaternion: {np.mean(np.array(d4_ori))}')
+    # print(
+    #     f'3 : TP-GMM: This distance at the start: {np.mean(np.array(d4_pos), axis=0)[0]} mm, end: {np.mean(np.array(d4_pos), axis=0)[1]}, average: {np.mean(np.array(d4_pos), axis=0)[-1]} mm, average quaternion: {np.mean(np.array(d4_ori))}')
