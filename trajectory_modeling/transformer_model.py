6fa406367e92569f1298615b1c3ada8b49fb10cc
cyinghan <cyinghan0320@gmail.com>
2023-09-14 09:25:14 -0400

    add transformer script

--- trajectory_modeling/transformer_model.py
+++ trajectory_modeling/transformer_model.py
@@ -63,3 +63,88 @@ class TFModelLite(nn.Module):
         x = self.tf(obj_emb, traj_emb)
         x = self.lin4(x)
         return x
+    
+class TFEncoderDecoder(nn.Module):
+    def __init__(self, task_dim:int, traj_dim: int, embed_dim: int, nhead: int, max_len: int,
+                 num_encoder_layers: int, num_decoder_layers: int, dropout: float = 0.2, device=None):
+        super(TFEncoderDecoder, self).__init__()
+        self.d_model = embed_dim
+        self.pos_embed = PositionalEncoding(embed_dim, dropout)
+        self.num_encoder_layers = num_encoder_layers
+        self.num_decoder_layers = num_decoder_layers
+        if num_encoder_layers > 0:
+            e_layer = nn.TransformerEncoderLayer(d_model=embed_dim, nhead=nhead, batch_first=True, dtype=torch.float64)
+            self.encoder = nn.TransformerEncoder(e_layer, num_layers=self.num_encoder_layers) 
+        
+        d_layer = nn.TransformerDecoderLayer(d_model=embed_dim, nhead=nhead, batch_first=True, dtype=torch.float64)
+        self.decoder = nn.TransformerDecoder(decoder_layer=d_layer, num_layers=self.num_decoder_layers)
+        self.lin0 = nn.Linear(task_dim, embed_dim, dtype=torch.float64)
+        self.lin4 = nn.Linear(embed_dim, traj_dim, dtype=torch.float64)
+        
+        self.mask = torch.triu(torch.full((max_len, max_len), True), diagonal=1)
+        if device:
+            self.to(device)
+            self.mask = torch.triu(torch.full((max_len, max_len), True, device=device), diagonal=1)
+        
+    def forward(self, obj_seq, traj_seq, key_mask=None):
+        obj_emb = self.lin0(obj_seq) * math.sqrt(self.d_model)
+        traj_emb = self.lin0(traj_seq) * math.sqrt(self.d_model)
+        traj_emb = self.pos_embed(traj_emb)
+        if self.num_encoder_layers > 0:
+            obj_emb = self.encoder(obj_emb)
+        x = self.decoder(traj_emb, obj_emb, tgt_mask=self.mask, tgt_key_padding_mask=key_mask)
+        x = self.lin4(x)
+        return x
+    
+class TFEncoderDecoderNoMask(nn.Module):
+    def __init__(self, task_dim:int, traj_dim: int, embed_dim: int, nhead: int, max_len: int,
+                 num_encoder_layers: int, num_decoder_layers: int, dropout: float = 0.2, device=None):
+        super(TFEncoderDecoderNoMask, self).__init__()
+        self.d_model = embed_dim
+        self.pos_embed = PositionalEncoding(embed_dim, dropout)
+        self.num_encoder_layers = num_encoder_layers
+        self.num_decoder_layers = num_decoder_layers
+        if num_encoder_layers > 0:
+            e_layer = nn.TransformerEncoderLayer(d_model=embed_dim, nhead=nhead, batch_first=True, dtype=torch.float64)
+            self.encoder = nn.TransformerEncoder(e_layer, num_layers=self.num_encoder_layers) 
+        
+        d_layer = nn.TransformerDecoderLayer(d_model=embed_dim, nhead=nhead, batch_first=True, dtype=torch.float64)
+        self.decoder = nn.TransformerDecoder(decoder_layer=d_layer, num_layers=self.num_decoder_layers)
+        self.lin0 = nn.Linear(task_dim, embed_dim, dtype=torch.float64)
+        self.lin4 = nn.Linear(embed_dim, traj_dim, dtype=torch.float64)
+        
+        self.mask = torch.triu(torch.full((max_len, max_len), True), diagonal=1)
+        if device:
+            self.to(device)
+            self.mask = torch.triu(torch.full((max_len, max_len), True, device=device), diagonal=1)
+        
+    def forward(self, obj_seq, traj_seq, key_mask=None):
+        obj_emb = self.lin0(obj_seq) * math.sqrt(self.d_model)
+        traj_emb = self.lin0(traj_seq) * math.sqrt(self.d_model)
+        traj_emb = self.pos_embed(traj_emb)
+        if self.num_encoder_layers > 0:
+            obj_emb = self.encoder(obj_emb)
+        x = self.decoder(traj_emb, obj_emb)
+        x = self.lin4(x)
+        return x
+
+    
+class TFDecoder(nn.Module):
+    def __init__(self, task_dim:int, traj_dim: int, embed_dim: int, nhead: int,
+                 layers: int, dropout: float = 0.2):
+        super(TFDecoder, self).__init__()
+        self.d_model = embed_dim
+        self.pos_embed = PositionalEncoding(embed_dim, dropout)
+        dc_layer = nn.TransformerDecoderLayer(d_model=embed_dim, nhead=nhead, batch_first=True, dtype=torch.float64)
+        self.decoder = nn.TransformerDecoder(decoder_layer=dc_layer, num_layers=layers)
+        self.lin0 = nn.Linear(task_dim, embed_dim, dtype=torch.float64)
+        self.lin4 = nn.Linear(embed_dim, traj_dim, dtype=torch.float64)
+        
+    def forward(self, obj_seq, traj_seq):
+        obj_emb = self.lin0(obj_seq) * math.sqrt(self.d_model)
+        traj_emb = self.lin0(traj_seq) * math.sqrt(self.d_model)
+        traj_emb = self.pos_embed(traj_emb)
+        
+        x = self.decoder(traj_emb, obj_emb)
+        x = self.lin4(x)
+        return x
